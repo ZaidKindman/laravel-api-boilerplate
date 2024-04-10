6f0fd644d119b4b082dd7b7430484f5989dc036d
Zaid Kindman <zaid1997uk@gmail.com>
2024-04-10 11:45:09 +0300

    modify change todo state request

--- app/Http/Requests/Todo/ChangeTodoStateRequest.php
+++ app/Http/Requests/Todo/ChangeTodoStateRequest.php
@@ -22,6 +22,7 @@ class ChangeTodoStateRequest extends FormRequest
     public function rules(): array
     {
         return [
+            'id' => ['required'],
             'completed' => ['required', 'boolean'],
         ];
     }
