b1ccc980a40431b00a9c12b934b0a8b23fe02d94
Zaid Kindman <zaid1997uk@gmail.com>
2024-04-13 14:53:18 +0300

    add names to api routes

--- routes/api.php
+++ routes/api.php
@@ -9,15 +9,15 @@ Route::get('/', function () {
 });
 
 Route::prefix('auth')->group(function () {
-    Route::post('login', [AuthController::class, 'login']);
-    Route::post('register', [AuthController::class, 'register']);
-    Route::post('logout', [AuthController::class, 'logout'])->middleware('auth');
+    Route::post('login', [AuthController::class, 'login'])->name('auth.login');
+    Route::post('register', [AuthController::class, 'register'])->name('auth.register');
+    Route::post('logout', [AuthController::class, 'logout'])->middleware('auth')->name('auth.logout');
 });
 
 Route::prefix('todos')->middleware('auth')->group(function () {
-    Route::post('/', [TodoController::class, 'store'])->middleware('permission:create todo');
-    Route::patch('/changeTodoState', [TodoController::class, 'changeTodoState'])->middleware('permission:edit todo');
-    Route::put('/', [TodoController::class, 'update'])->middleware('permission:edit todo');
-    Route::get('/', [TodoController::class, 'index'])->middleware('permission:view todo');
-    Route::delete('/', [TodoController::class, 'delete'])->middleware('permission:delete todo');
+    Route::post('/', [TodoController::class, 'store'])->middleware('permission:create todo')->name('todo.store');
+    Route::patch('/changeTodoState', [TodoController::class, 'changeTodoState'])->middleware('permission:edit todo')->name('todo.changeTodoState');
+    Route::put('/', [TodoController::class, 'update'])->middleware('permission:edit todo')->name('todo.edit');
+    Route::get('/', [TodoController::class, 'index'])->middleware('permission:view todo')->name('todo.view');
+    Route::delete('/', [TodoController::class, 'delete'])->middleware('permission:delete todo')->name('todo.delete');
 });
